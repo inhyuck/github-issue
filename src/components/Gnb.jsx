e59359a4dec07da828081900d48fdb279a1f58b6
inhyuck <dlsgur1447@gmail.com>
2021-04-22 01:24:00 +0900

    feat: switch menu (label or milestone)

--- src/components/Gnb.jsx
+++ src/components/Gnb.jsx
@@ -1,11 +1,20 @@
-export default function Gnb() {
+import {MENU} from './consts/menu.js';
+import {toPascalCase} from './utils/stringUtils.js';
+
+export default function Gnb({menu: selectedMenu, onChangeMenu}) {
+    const selectedButtonStyle = {
+        backgroundColor: 'rgb(3, 102, 214)',
+    };
+    const menuButtons = Object.values(MENU)
+                              .map(menu => <button key={menu} style={selectedMenu === menu ? selectedButtonStyle : {}}
+                                                   onClick={() => onChangeMenu(menu)}>{toPascalCase(menu)}</button>);
+
     return (
         <nav className="gnb">
             <div className="tab-buttons">
-                <button>Labels</button>
-                <button>Milestones</button>
+                {menuButtons}
             </div>
-            <button className="new-btn">New Label</button>
+            <button className="new-btn">{`New ${selectedMenu}`}</button>
         </nav>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,21 @@
+import React from 'react';
+
 import Gnb from './Gnb.jsx';
 import LabelsRowWrap from './labels/LabelsRowWrap.jsx';
+import {MENU} from './consts/menu.js';
+import MilestonesRowWrap from './milestones/MilestonesRowWrap.jsx';
 
 export default function Main() {
+    const [menu, setMenu] = React.useState(MENU.LABELS);
+
+    const onChangeMenu = (menu) => setMenu(() => menu);
+
+    const rowWrap = menu === MENU.LABELS ? <LabelsRowWrap/> : <MilestonesRowWrap/>;
+
     return (
         <main className="contents">
-            <Gnb/>
-            <LabelsRowWrap/>
-            {/*<MilestonesRowWrap/>*/}
+            <Gnb menu={menu} onChangeMenu={onChangeMenu}/>
+            {rowWrap}
         </main>
     );
 }
