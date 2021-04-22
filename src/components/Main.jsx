a96572e5b03ac442d010d32c565e64d97bad118a
inhyuck <dlsgur1447@gmail.com>
2021-04-23 00:20:42 +0900

    feat: Edit label subject and description (incomplete)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Gnb from './Gnb.jsx';
 import LabelsRowWrap from './labels/LabelsRowWrap.jsx';
@@ -7,14 +7,14 @@ import MilestonesRowWrap from './milestones/MilestonesRowWrap.jsx';
 import APIs from '../apis/APIs.js';
 
 export default function Main() {
-    const [menu, setMenu] = React.useState(MENU.LABELS);
-    const [labels, setLabels] = React.useState([]);
+    const [menu, setMenu] = useState(MENU.LABELS);
+    const [labels, setLabels] = useState([]);
 
     const fetchLabels = () => {
         APIs.getLabels()
             .then(labels => setLabels(labels));
     };
-    React.useEffect(fetchLabels, []);
+    useEffect(fetchLabels, []);
 
     const onChangeMenu = (menu) => setMenu(() => menu);
 
--- src/components/labels/LabelsRowList.jsx
+++ src/components/labels/LabelsRowList.jsx
@@ -1,7 +1,13 @@
 import LabelsRowItem from './LabelsRowItem.jsx';
+import LabelEditWrap from './LabelEditWrap.jsx';
 
 export default function LabelsRowList({labels}) {
-    const labelsRowList = labels.map((label) => <LabelsRowItem key={label.id} label={label}/>);
+    const labelsRowList = labels.map((label) => {
+        if (label.id === 1) { //todo editing 상태 따로 관리
+            return <LabelEditWrap key={label.id} label={labels[0]}/>;
+        }
+        return <LabelsRowItem key={label.id} label={label}/>;
+    });
 
     return (
         <div className="row-list">
