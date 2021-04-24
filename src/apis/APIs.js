cce36fb200321aa565271dfa89c90c9f6715de08
inhyuck <dlsgur1447@gmail.com>
2021-04-24 19:02:54 +0900

    refactor: Extract environment valuable - API end point

--- src/apis/APIs.js
+++ src/apis/APIs.js
@@ -1,7 +1,7 @@
-const END_POINT = 'http://localhost:3001';
+const API_END_POINT = process.env.REACT_APP_API_END_POINT;
 
 const GET = (url) => {
-    return fetch(`${END_POINT}${url}`)
+    return fetch(`${API_END_POINT}${url}`)
         .then(response => {
             return response.json();
         });
