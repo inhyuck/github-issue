b720b6dcc0e20496e3f9263a12673e665920eab6
inhyuck <dlsgur1447@gmail.com>
2021-04-26 00:30:21 +0900

    refactor: Error Processing server APIs

--- src/apis/APIs.js
+++ src/apis/APIs.js
@@ -1,23 +1,31 @@
 const API_END_POINT = process.env.REACT_APP_API_END_POINT;
 
-const GET = (url) => {
-    return fetch(`${API_END_POINT}${url}`)
-        .then(response => {
-            return response.json();
-        });
-};
+const defaultAPIs = (method, url, data) => {
+    let config = {method};
+    if (method === 'POST' || method === 'PUT') {
+        config = {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }
+    }
 
-const PUT = (url, data) => {
-    return fetch(`${API_END_POINT}${url}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    }).then(response => {
+    return fetch(`${API_END_POINT}${url}`, config).then(response => {
+        if (!response.ok) {
+            throw new Error('response is not OK');
+        }
         return response.json();
+
+    }).catch(error => {
+        throw new Error(error);
     });
 };
+
+const GET = (url) => defaultAPIs('GET', url);
+const POST = (url, data) => defaultAPIs('POST', url, data);
+const PUT = (url, data) => defaultAPIs('PUT', url, data);
 
 const APIs = {
     getLabels() {
